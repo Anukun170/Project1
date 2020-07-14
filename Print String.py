c4acaa9f91e27a89b3be7fc2762126c4be13ecb4
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-14 16:50:20 +0700

    Save String

---
+++ Print String.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Information and Network Engineering")
+print('He ask me"Who got the book"')
+print("I'm hungry")
+print("I'm good and \ i will do my best\"")
+print('/\\')
+print('\ta\ta\ta\n\ta')
+print('''\ta\ta\ta\ta\ta\ta''')
+print('''\ta
+a\ta\ta
+\ta''')
